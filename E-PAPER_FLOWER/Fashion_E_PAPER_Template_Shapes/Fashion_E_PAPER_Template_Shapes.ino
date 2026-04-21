adec655135d518edf5d6fce4a0af98a2fe175c48
GoochTrimmer <77869307+GoochTrimmer@users.noreply.github.com>
2026-04-22 02:00:00 +0800

    Added new BitMap graphics
    Added logo and name initial graphics

--- E-PAPER_FLOWER/Fashion_E_PAPER_Template_Shapes/Fashion_E_PAPER_Template_Shapes.ino
+++ E-PAPER_FLOWER/Fashion_E_PAPER_Template_Shapes/Fashion_E_PAPER_Template_Shapes.ino
@@ -1,6 +1,8 @@
 #include <GxEPD2_3C.h>
 #include <SPI.h>
 #include "flower_bitmaps.h"
+#include "logo_bitmaps.h"
+#include "x_bitmaps.h"
 
 #define EPD_CS   0
 #define EPD_DC   1
@@ -18,6 +20,7 @@ void setup()
 {
   Serial.begin(115200);
   delay(1000);
+  Serial.println("Starting logo and flower test...");
 
   SPI.begin(EPD_SCK, -1, EPD_MOSI, EPD_CS);
 
@@ -25,6 +28,33 @@ void setup()
   display.setRotation(0);
   display.setFullWindow();
 
+  Serial.println("Displaying X...");
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);  // Clear the screen
+    
+    display.drawBitmap(0, 0, xBitmap, X_W, X_H, GxEPD_BLACK);  // Draw the logo
+  } while (display.nextPage());
+  Serial.println("X displayed. Waiting for 5 seconds...");
+  delay(5000);  // Wait for 5 seconds
+
+  // Step 1: Show the logo bitmap
+  Serial.println("Displaying logo...");
+  display.firstPage();
+  do {
+    display.fillScreen(GxEPD_WHITE);  // Clear the screen
+
+    // Calculate the center position (X and Y)
+    int x_offset = (200 - LOGO_W) / 2; // X offset to center horizontally
+    int y_offset = (200 - LOGO_H) / 2; // Y offset to center vertically
+    
+    display.drawBitmap(x_offset, y_offset, logoBitmap, LOGO_W, LOGO_H, GxEPD_BLACK);  // Draw the logo
+  } while (display.nextPage());
+  Serial.println("Logo displayed. Waiting for 5 seconds...");
+  delay(5000);  // Wait for 5 seconds
+
+  // Step 2: show the flower bitmap
+  Serial.println("Displaying flower...");
   display.firstPage();
   do {
     display.fillScreen(GxEPD_WHITE);
